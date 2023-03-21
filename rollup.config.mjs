d58dc212bf8fdf12335543146483f9ac45361c3e
Yngwie Kurebayashi <yngwie.rs2@gmail.com>
2023-03-21 02:12:57 -0300

    adds style

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from './package.json' assert { type: 'json'};
+import postcss from 'rollup-plugin-postcss';
 
 export default [
     {
@@ -23,11 +24,13 @@ export default [
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
+            postcss()
         ],
     },
     {
         input: "dist/esm/types/index.d.ts",
         output: [{ file: "dist/index.d.ts", format: "esm" }],
         plugins: [dts()],
+        external: [/\.css$/],
     },
 ];
